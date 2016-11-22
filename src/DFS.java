dfdf5ffaa02c679efc98335043427d6b21cf7e37
cestrame <github@extrasmisc.com>
2016-11-22 15:50:37 -0500

    dfs

--- src/DFS.java
+++ src/DFS.java
@@ -1,6 +1,8 @@
 
 public static class DFS{
 	
-	public static ArrayList DFSearch(pathNode	
+	public static ArrayList DFSearch(pathNode startNode, Arraylist<puzzleNode> nodeSet){
+		String index = Integer.toBinaryString(nodeSet.indexOf(startNode));
+		
 	
 }

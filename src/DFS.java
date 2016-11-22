fa3aebe3250e95224624977d8d37a3e81d415147
Zoey16 <youxiang1998@live.cn>
2016-11-22 16:09:03 -0500

    add comment
    I added some comments

--- src/DFS.java
+++ src/DFS.java
@@ -1,8 +1,9 @@
-
+//add comments
 public static class DFS{
 	
 	public static ArrayList DFSearch(pathNode startNode, Arraylist<puzzleNode> nodeSet){
 		String index = Integer.toBinaryString(nodeSet.indexOf(startNode));
+		
 		
 	
 }
